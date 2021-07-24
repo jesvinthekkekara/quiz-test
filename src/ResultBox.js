dd3f985a3f384981ddc073cdf6e186992b75639e
Jesvin123 <jesvinthekkekara.jt@gmail.com>
2021-07-24 12:00:16 +0530

    alpha 1.4

--- src/ResultBox.js
+++ src/ResultBox.js
@@ -1,13 +1,26 @@
 import React from "react";
 import "./ResultBox.css";
 import { Link } from "react-router-dom";
-
+import logo from "./images/quiz2.gif";
 function ResultBox({ score }) {
   console.log("score in resultbox:", score);
   return (
     <div className="score-board">
       <div className="score">
-        <h3>Your score is {score?score:0} / 5 correct answer ! ! !</h3>
+        <div className="score_image">
+          <img
+            style={{
+              height: "200px",
+              width: "200px",
+              textAlign: " center",
+              display: "block",
+              marginLeft: "auto",
+              marginRight: "auto",
+            }}
+            src={logo}
+          />
+        </div>
+        <h3>Your score is {score ? score : 0} / 5 correct answer ! ! !</h3>
       </div>
       <div className="result_button">
         <Link to="/playagain">
